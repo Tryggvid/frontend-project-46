a908b0be58c1e526440af9372afaa8994c4d1372
boba <a.alexsandershliago@yandex.ru>
2026-06-28 21:47:26 +0300

    ci: add coverage and sonar configuration

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,10 @@ export default {
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/**/*.test.js',
+  ],
+  coverageReporters: ['text', 'lcov', 'clover'],
+  coverageDirectory: 'coverage',
 };
